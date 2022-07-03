e788c1d01b122aec24cd90c415018b882f5b4d34
yurrikon <yurrikon@gmail.com>
2022-07-03 03:33:32 +0300

    Task50 completed

--- Task54.cs
+++ Task54.cs
@@ -6,35 +6,69 @@ Console.WriteLine();
 int[,] array = new int[4,5];
 Random rand = new Random();
 
+FillArray(array);
+PrintArray(array);
+SortRowArray(array);
+PrintArraySorted(array);
 
-
-void SortArray(int[,] array)
+void FillArray(int[,] array)
 {
-    for (int i = 0; i < array.GetLength(0) - 1; i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            int minPosition = j;
-            for (int k = j = 1; k < array.GetLength(1); k++)
-            {
-                if (array[i,j] < array[i,minPosition])
-                {
-                    minPosition = j;
-                }
-            }
-            int temporary = array[i,j];
-            array[i,j] = array[i,minPosition];
-            array[i,minPosition] = temporary;
+            array[i,j] = rand.Next(1, 100);
         }
     }
 }
-void PrintArraySorted(int[,] array)
+
+void PrintArray(int[,] array)
 {
+    Console.WriteLine("Заданный массив:");
+    Console.WriteLine();
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
             Console.Write(array[i,j] + "\t");
         }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
+
+void SortRowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int minPosition = j;
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i,k] < array[i,minPosition])
+                {
+                    minPosition = k;
+                }
+            }
+                int temporary = array[i,j];
+                array[i,j] = array[i,minPosition];
+                array[i,minPosition] = temporary;
+        }
+    }
+}
+
+void PrintArraySorted(int[,] array)
+{
+    Console.WriteLine("Массив, отсортированный построчно:");
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
     }
 }
