c7400622d53bd2373692780e7fcf62648fcf4579
yurrikon <yurrikon@gmail.com>
2022-07-09 15:10:26 +0300

    ask56 completed

--- Task56.cs
+++ Task56.cs
@@ -1,13 +1,13 @@
 // Задача 56: Задайте прямоугольный двумерный массив. 
 // Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 
+Console.WriteLine();
 int[,] array = new int[4,5];
 Random rand = new Random();
 
 FillArray(array);
 PrintArray(array);
-FindMinRowArray(array);
-/* PrintResult(array); */
+FindMinRow(array);
 
 void FillArray(int[,] array)
 {
@@ -35,42 +35,29 @@ void PrintArray(int[,] array)
     }
 }
 
-void FindMinRowArray(int[,] array)
+void FindMinRow(int[,] array)
 {
-    int sumrow = 0;
-    int summin = 0;
     int minrow = 0;
+    int summin = 0;
+    int sumtemp = 0;
+    
+
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        
+        int sumrow = 0;
         for (int j = 0; j < array.GetLength(1); j++)
         {
             sumrow += array[i,j];
         }
-        sumrow = summin;
-        if (sumrow < summin) 
+        Console.WriteLine($"Сумма {i+1} строки: {sumrow}");
+        if (sumrow < sumtemp)
         {
             summin = sumrow;
+            minrow = i;
         }
-        
-        
+        sumtemp = sumrow;
     }
-    Console.WriteLine(summin);
-    Console.WriteLine(minrow);
-}
-
-/* void PrintResult(int[,] array)
-{
-    Console.WriteLine("Отсортированный массив:");
     Console.WriteLine();
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i,j] + "\t");
-        }
-        Console.WriteLine();
-        Console.WriteLine();
-    }
+    Console.WriteLine($"Наименьшаяя сумма = {summin}");
+    Console.WriteLine($"Номер строки = {minrow+1}");
 }
-*/
